35208f64b1f334e838f7bc5663d535192981bede
Ralph Bean <rbean@redhat.com>
2012-04-19 18:30:52 -0400

    Basic model.

--- tahrir/models.py
+++ tahrir/models.py
@@ -1,28 +1,94 @@
+import hashlib
 from sqlalchemy import (
     Column,
     Integer,
-    Text,
-    )
+    DateTime,
+    Unicode,
+    ForeignKey,
+)
 
 from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import (
     scoped_session,
     sessionmaker,
-    )
+    relationship,
+    backref,
+)
 
 from zope.sqlalchemy import ZopeTransactionExtension
 
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
-Base = declarative_base()
+DeclarativeBase = declarative_base()
 
-class MyModel(Base):
-    __tablename__ = 'models'
+
+class Issuer(DeclarativeBase):
+    __tablename__ = 'issuers'
     id = Column(Integer, primary_key=True)
-    name = Column(Text, unique=True)
-    value = Column(Integer)
+    origin = Column(Unicode(128), nullable=False)
+    name = Column(Unicode(128), nullable=False)
+    org = Column(Unicode(128), nullable=False)
+    contact = Column(Unicode(128), nullable=False)
+    badges = relationship("Badge", backref="issuer")
 
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
+    def __json__(self):
+        return dict(
+            origin=self.origin,
+            name=self.name,
+            org=self.org,
+            contact=self.contact,
+        )
 
+
+class Badge(DeclarativeBase):
+    __tablename__ = 'badges'
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode(128), nullable=False)
+    image = Column(Unicode(128), nullable=False)
+    description = Column(Unicode(128), nullable=False)
+    criteria = Column(Unicode(128), nullable=False)
+    assertions = relationship("Assertion", backref="badge")
+    issuer_id = Column(Integer, ForeignKey('issuers.id'))
+
+    def __json__(self):
+        return dict(
+            version="0.5.0",
+            name=self.name,
+            image=self.image,
+            description=self.description,
+            criteria=self.criteria,
+            issuer=self.issuer.__json__(),
+        )
+
+
+class Person(DeclarativeBase):
+    __tablename__ = 'persons'
+    id = Column(Integer, primary_key=True)
+    email = Column(Unicode(128), nullable=False)
+    assertions = relationship("Assertion", backref="person")
+
+
+class Assertion(DeclarativeBase):
+    __tablename__ = 'assertions'
+    id = Column(Integer, primary_key=True)
+    badge_id = Column(Integer, ForeignKey('badges.id'))
+    person_id = Column(Integer, ForeignKey('persons.id'))
+    salt = Column(Unicode(128), nullable=False)
+    issued_on = Column(DateTime)
+
+    @property
+    def recipient(self):
+        return "sha256$%s" % \
+            hashlib.sha256(self.person.email + self.salt).hexdigest()
+
+    def __json__(self):
+        result = dict(
+            recipient=self.recipient,
+            salt=self.salt,
+            badge=self.badge.__json__(),
+        )
+
+        if self.issued_on:
+            result['issued_on'] = self.issued_on.strftime("%Y-%m-%d")
+
+        return result
--- tahrir/scripts/initializedb.py
+++ tahrir/scripts/initializedb.py
@@ -1,4 +1,7 @@
+
+import datetime
 import os
+import pprint
 import sys
 import transaction
 
@@ -7,29 +10,61 @@ from sqlalchemy import engine_from_config
 from pyramid.paster import (
     get_appsettings,
     setup_logging,
-    )
+)
 
 from ..models import (
     DBSession,
-    MyModel,
-    Base,
-    )
+    Issuer,
+    Badge,
+    Person,
+    Assertion,
+    DeclarativeBase,
+)
 
 def usage(argv):
     cmd = os.path.basename(argv[0])
     print('usage: %s <config_uri>\n'
-          '(example: "%s development.ini")' % (cmd, cmd)) 
+          '(example: "%s development.ini")' % (cmd, cmd))
     sys.exit(1)
 
 def main(argv=sys.argv):
     if len(argv) != 2:
         usage(argv)
+
     config_uri = argv[1]
     setup_logging(config_uri)
     settings = get_appsettings(config_uri)
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
-    Base.metadata.create_all(engine)
+    DeclarativeBase.metadata.create_all(engine)
     with transaction.manager:
-        model = MyModel(name='one', value=1)
-        DBSession.add(model)
+        issuer = Issuer(
+            name="Ralph Bean",
+            origin="http://badges.threebean.org",
+            org="threebean.org",
+            contact="rbean@redhat.com",
+        )
+        DBSession.add(issuer)
+        badge = Badge(
+            name="Plus One!",
+            image="/pngs/threebean-plus-one.png", # TODO -- wat?
+            description="""
+            Got a recommendation from threebean for being awesome.
+            """.strip(),
+            criteria="/badges/plus-one",  # TODO -- how should this work?
+            issuer=issuer,
+        )
+        DBSession.add(badge)
+        person = Person(
+            email="rbean@redhat.com",
+        )
+        DBSession.add(person)
+        assertion = Assertion(
+            badge=badge,
+            person=person,
+            salt="beefy",  # TODO -- pull from config
+            issued_on=datetime.datetime.now(),
+        )
+        DBSession.add(assertion)
+
+        pprint.pprint(assertion.__json__())
