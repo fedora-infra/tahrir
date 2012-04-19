ed00c0f9a006cf18f2a32f068d72a5d93be29b33
Ralph Bean <rbean@redhat.com>
2012-04-19 18:15:06 -0400

    pcreate -s alchemy tahrir.

---
+++ README.txt
@@ -0,0 +1,14 @@
+tahrir README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $venv/bin/python setup.py develop
+
+- $venv/bin/populate_tahrir development.ini
+
+- $venv/bin/pserve development.ini
+
---
+++ setup.py
@@ -0,0 +1,45 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'SQLAlchemy',
+    'transaction',
+    'pyramid_tm',
+    'pyramid_debugtoolbar',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='tahrir',
+      version='0.0',
+      description='tahrir',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='tahrir',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = tahrir:main
+      [console_scripts]
+      initialize_tahrir_db = tahrir.scripts.initializedb:main
+      """,
+      )
+
