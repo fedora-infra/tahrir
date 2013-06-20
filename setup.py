fdaaf1679ea21c095e811576312430bdc559850c
Ralph Bean <rbean@redhat.com>
2013-06-20 14:52:08 -0400

    Bump our version constraint for tahrir_api.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 
 requires = [
     'pyramid',
-    'tahrir_api >= 0.1.4.0',
+    'tahrir_api >= 0.2.0',
     'SQLAlchemy',
     'transaction',
     'pyramid_tm',
