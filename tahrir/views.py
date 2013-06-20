9e83a49ad24f33549c41e55a9b7a5f392cccbc45
David Gay <oddshocks@riseup.net>
2013-06-20 12:32:11 -0400

    Remove some comments and debugging statements. Thanks @ralphbean!

--- tahrir/views.py
+++ tahrir/views.py
@@ -62,16 +62,10 @@ def index(request):
     request.session['came_from'] = '/'
 
     persons_assertions = request.db.get_all_assertions().join(m.Person)
-    #persons_assertions = request.db.get_all_persons().outerjoin(m.Assertion)
     from collections import defaultdict
     top_persons = defaultdict(int) # person_id: assertion count
     for item in persons_assertions:
         top_persons[item.person.email] += 1
-    # TODO: Make sure this top_persons stuff actually sorts properly. :P
-    # I can't check easily right now, since all other views are busted
-    # during this database rewiring.
-    from q import q
-    q(persons_assertions)
     return dict(
         auth_principals=effective_principals(request),
         latest_awards=request.db.get_all_assertions().order_by(
