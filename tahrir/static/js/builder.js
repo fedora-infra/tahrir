7bff75e00c95a528944adfdbe4ce8ead9ae2d9bf
David Gay <oddshocks@riseup.net>
2013-06-23 18:07:51 -0400

    All sections in badge builder operating as expected.

--- tahrir/static/js/builder.js
+++ tahrir/static/js/builder.js
@@ -5,6 +5,12 @@ $(document).ready(function() {
     var badgeName = $('input[name=badge-name]').val();
     var badgeDescription = $('input[name=badge-description]').val();
     var badgeCreator = $('input[name=badge-creator]').val();
+    var discussion = $('input[name=discussion]').val();
+    var image = $('input[name=image]').val();
+    var issuer = $('input[name=issuer]').val();
+    var trigger = $('input[name=trigger]').val();
+    var triggerTopic = $('input[name=triggerTopic]').val();
+    var criteria = $('input[name=criteria]').val();
 
     function generateOutput() {
         return "%YAML 1.2\n"
@@ -16,13 +22,34 @@ $(document).ready(function() {
              + "creator:        " + badgeCreator + "\n"
              + "\n"
              + "# This is a link to the discussion about adopting this as "
-             + "a for-real badge.\n";
+             + "a for-real badge.\n"
+             + "discussion:     " + discussion + "\n"
+             + "\n"
+             + "# A link to the image for the badge.\n"
+             + "image_url:      " + image + "\n"
+             + "\n"
+             + "# The issuer.\n"
+             + "issuer_id:      " + issuer + "\n"
+             + "\n"
+             + "# We'll perform our more costly check (defined below)\n"
+             + "# only when we receive messages that match this trigger.\n"
+             + "trigger:        " + trigger + "\n"
+             + "  topic:        " + triggerTopic + "\n"
+             + "\n"
+             + "# Once the check has been triggered, this defines what we"
+             + " actually check.\n"
+             + "criteria:       " + criteria + "\n"
+             // TODO: Figure out best way to add criteria here.
+             + "(This section is under construction.)";
+
     }
 
     function updateTextarea() {
         $('textarea#preview').val(generateOutput());
     }
-                   
+
+    // We call this once to make sure things are proper on page load.
+    updateTextarea();
 
     $('input[name=badge-name]').keyup(function() {
         badgeName = $(this).val();
@@ -34,8 +61,33 @@ $(document).ready(function() {
         updateTextarea();
     });
 
-    $('input[name=badge-creator]').keyup(function() {
-        badgeCreator = $(this).val();
+    $('input[name=discussion]').keyup(function() {
+        discussion = $(this).val();
+        updateTextarea();
+    });
+
+    $('input[name=image]').keyup(function() {
+        image = $(this).val();
+        updateTextarea();
+    });
+
+    $('input[name=issuer]').keyup(function() {
+        issuer = $(this).val();
+        updateTextarea();
+    });
+
+    $('input[name=trigger]').keyup(function() {
+        trigger = $(this).val();
+        updateTextarea();
+    });
+
+    $('input[name=triggerTopic]').keyup(function() {
+        triggerTopic = $(this).val();
+        updateTextarea();
+    });
+
+    $('input[name=criteria]').keyup(function() {
+        criteria = $(this).val();
         updateTextarea();
     });
 });
--- tahrir/templates/builder.mak
+++ tahrir/templates/builder.mak
@@ -24,11 +24,24 @@
 		<legend>Badge Builder</legend>
 		<table>
 		<tr><td><label for="badge-name">Badge Name</label></td>
-			<td><input name="badge-name" /></td></tr>
+			<td><input name="badge-name" tabindex="1" /></td>
+			<td><label for="discussion">Discussion URL</label></td>
+			<td><input name="discussion" tabindex="4" /></td></tr>
 		<tr><td><label for="badge-description">Badge Description</label></td>
-			<td><input name="badge-description" /></td></tr>
+			<td><input name="badge-description" tabindex="2" /></td>
+			<td><label for="image">Image URL</label></td>
+			<td><input name="image" tabindex="5" /></td></tr>
 		<tr><td><label for="badge-creator">Badge Creator</label></td>
-			<td><input name="badge-creator" /></td></tr>
+			<td><input name="badge-creator" tabindex="3" /></td>
+			<td><label for="issuer">Issuer ID</label></td>
+			<td><input name="issuer" tabindex="6" /></td></tr>
+		<tr><td><label for="trigger">Trigger</label></td>
+			<td><input name="trigger" tabindex="7" />
+			<td><label for="criteria">Criteria</label></td>
+			<td><input name="criteria" tabindex="9" /></td></tr>
+		<tr>
+			<td><label for="triggerTopic">Trigger Topic</label></td>
+			<td><input name="triggerTopic" tabindex="8" /></td></tr>
 		</table>
 		<h3>Preview</h3>
 		<textarea id="preview"
