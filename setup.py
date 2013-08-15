3a28f48496ffed97a34a9e90eab165bb94f600ae
Ralph Bean <rbean@redhat.com>
2013-08-15 17:13:26 -0400

    0.3.5

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ requires = [
     ]
 
 setup(name='tahrir',
-      version='0.3.4',
+      version='0.3.5',
       description='A pyramid app for issuing your own Open Badges',
       long_description=README,
       license="AGPLv3+ with additional permission",
