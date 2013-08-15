e2abb21c7f607eda1aaac5074bfff089bb9d3409
Ralph Bean <rbean@redhat.com>
2013-08-15 09:49:13 -0400

    Just to simplify.

--- tahrir/events.py
+++ tahrir/events.py
@@ -21,11 +21,13 @@ def inject_globals(event):
     # in like so...
     request = event['request']
 
+    settings = request.registry.settings
+
     # ... and then set a couple global variables that will be available
     # in every template, so we don't have to pass them through the
     # dict returned by the view every time!
-    event['title'] = request.registry.settings['tahrir.title']
-    event['base_url'] = request.registry.settings['tahrir.base_url']
+    event['title'] = settings['tahrir.title']
+    event['base_url'] = settings['tahrir.base_url']
 
     event['logged_in'] = authenticated_userid(request)
     person = request.db.get_person(event['logged_in'])
