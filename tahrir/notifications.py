897c2aaa8bd64dac5c8b7e7dff3505d7cc055620
Ryan Lerch <rlerch@redhat.com>
2024-04-08 10:44:20 +1000

    send fedora messages rather than fedmsg
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- tahrir/notifications.py
+++ tahrir/notifications.py
@@ -1,20 +1,39 @@
+import logging
+import sys
+import traceback
+
+import backoff
 import pyramid.threadlocal
+from fedora_messaging import api as fm_api
+from fedora_messaging import exceptions as fm_exceptions
 from pyramid.settings import asbool
 
-# Optional.  Emit messages to the fedmsg bus.
-fedmsg = None
-try:
-    import fedmsg
-except ImportError:
-    pass
+log = logging.getLogger(__name__)
+
+
+def _backoff_hdlr(details):
+    log.warning(f"Publishing message failed. Retrying. {traceback.format_tb(sys.exc_info()[2])}")
+
+
+@backoff.on_exception(
+    backoff.expo,
+    (fm_exceptions.ConnectionException, fm_exceptions.PublishException),
+    max_tries=3,
+    on_backoff=_backoff_hdlr,
+)
+def _publish(message):
+    fm_api.publish(message)
 
 
 def callback(topic, msg):
     request = pyramid.threadlocal.get_current_request()
     settings = request.registry.settings
-    if fedmsg and asbool(settings.get("tahrir.use_fedmsg", False)):
-        fedmsg.publish(
-            modname="fedbadges",
-            topic=topic,
-            msg=msg,
-        )
+    if asbool(settings.get("tahrir.use_fedmsg", False)):
+        log.debug(f"Publishing fedoramessage topic`{topic}` with message `{msg}`")
+        message = fm_api.Message(topic=topic, body=msg)
+        try:
+            _publish(message)
+        except fm_exceptions.BaseException:
+            log.error(
+                f"Publishing message failed. Giving up. {traceback.format_tb(sys.exc_info()[2])}"
+            )
