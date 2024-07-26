2fdeda0e66c67acb8b99db21942eb37a6451d1b4
Aurélien Bompard <aurelien@bompard.org>
2024-07-27 00:30:12 +0200

    Drop Whitenoise, Flask can serve static files just fine
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- tahrir/views/__init__.py
+++ tahrir/views/__init__.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, Flask, render_template
+from flask.helpers import send_from_directory
 
 
 blueprint = Blueprint("tahrir", __name__)
@@ -10,3 +11,11 @@ def page_not_found(e):
 
 def internal_server_error(e):
     return render_template("500.html"), 500
+
+
+def add_static_view(app: Flask, directory: str, prefix: str, endpoint: str):
+    def _view(filename):
+        max_age = app.get_send_file_max_age(filename)
+        return send_from_directory(directory, filename, max_age=max_age)
+
+    app.add_url_rule(f"{prefix}/<path:filename>", endpoint=endpoint, view_func=_view)
