4b33f9f05590ec4c25fc9f4deac12d4e8826ea0b
Aurélien Bompard <aurelien@bompard.org>
2024-08-16 17:44:33 +0200

    Fix the error pages
    - don't put the text inside the image
    - add alt text
    - use a single error page for all the error codes
    
    Fixes: #352
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- tahrir/views/__init__.py
+++ tahrir/views/__init__.py
@@ -6,11 +6,29 @@ blueprint = Blueprint("tahrir", __name__)
 
 
 def page_not_found(e):
-    return render_template("404.html"), 404
+    title = "Oops! The page you are looking for doesn't exist."
+    message = (
+        "It might have been removed or had its name changed. "
+        "Please check the URL or return to the home page."
+    )
+    return (
+        render_template("error.html", error_code=404, error_title=title, error_message=message),
+        404,
+    )
 
 
 def internal_server_error(e):
-    return render_template("500.html"), 500
+    title = "Whoops! There's badness in the server internals."
+    message = (
+        "Please try refreshing the page or come back later. "
+        "If the issue persists, please open an "
+        '<a href="https://pagure.io/fedora-infrastructure/new_issue">'
+        "Infrastructure ticket<a>."
+    )
+    return (
+        render_template("error.html", error_code=500, error_title=title, error_message=message),
+        500,
+    )
 
 
 def add_static_view(app: Flask, directory: str, prefix: str, endpoint: str):
