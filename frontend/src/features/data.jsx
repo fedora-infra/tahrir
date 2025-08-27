c14c4dfd7a1bdcdaa9707beacf0d640845625105
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2025-08-27 07:59:18 +0530

    Purge the Material UI implementation
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- frontend/src/features/data.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import makeunitReducer from "./part.jsx";
-
-export const data = configureStore({
-  reducer: {
-    area: makeunitReducer,
-  },
-});
