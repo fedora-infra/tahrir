34eef5e626e14b00de82ff1395563ec19e281e85
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2025-09-01 10:26:40 +0530

    Include associated changes for rendering portraits
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- tahrir/views/explore.py
+++ tahrir/views/explore.py
@@ -113,7 +113,7 @@ def json_explore(search_query):
             "id": person.id,
             "bio": person.bio if person.bio else None,
             "created_on": person.created_on.timestamp() if person.created_on else None,
-            "email": person.email,
+            "email": person.avatar,
             "last_login": person.last_login.timestamp() if person.last_login else None,
             "nickname": person.nickname,
             "rank": person.rank,
