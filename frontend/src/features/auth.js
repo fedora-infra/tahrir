b300c0cebcb4999db850ce2066acf8a8ce0c38c4
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2025-09-26 10:33:37 +0530

    Create foundations for OIDC-based authentication
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

---
+++ frontend/src/features/auth.js
@@ -0,0 +1,48 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import md5 from "crypto-js/md5";
+
+import { userManager } from "../config/oidc.js";
+
+export const loadUserData = createAsyncThunk("auth/loadUserData", async () => {
+  const user = await userManager.getUser();
+  return user && !user.expired ? user.profile : null;
+});
+
+const authData = createSlice({
+  name: "auth",
+  initialState: {
+    user: null,
+    disp: "https://seccdn.libravatar.org/avatar/40f8d096a3777232204cb3f796c577b7?s=30",
+    status: "idle",
+  },
+  reducers: {
+    wipeUserData: (auth) => {
+      auth.user = null;
+      auth.disp = "https://seccdn.libravatar.org/avatar/40f8d096a3777232204cb3f796c577b7?s=30";
+      auth.status = "idle";
+      console.log("WIPED");
+    },
+  },
+  extraReducers: (plan) => {
+    plan
+      .addCase(loadUserData.pending, (auth) => {
+        auth.status = "load";
+      })
+      .addCase(loadUserData.fulfilled, (auth, action) => {
+        auth.user = action.payload;
+        auth.disp = action.payload
+          ? `https://seccdn.libravatar.org/avatar/${md5(action.payload.email.trim().toLowerCase()).toString()}?s=30&d=retro`
+          : "https://seccdn.libravatar.org/avatar/40f8d096a3777232204cb3f796c577b7?s=30";
+        auth.status = "pass";
+      })
+      .addCase(loadUserData.rejected, (auth) => {
+        auth.user = null;
+        auth.disp = "https://seccdn.libravatar.org/avatar/40f8d096a3777232204cb3f796c577b7?s=30";
+        auth.status = "fail";
+      });
+  },
+});
+
+export const { wipeUserData } = authData.actions;
+
+export default authData.reducer;
--- frontend/src/features/data.js
+++ frontend/src/features/data.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import authDataReducer from "./auth.js";
 import { callUnit } from "./call.js";
 import callUnitReducer from "./call.js";
 import makeUnitReducer from "./part.js";
@@ -7,6 +8,7 @@ import makeUnitReducer from "./part.js";
 export const data = configureStore({
   reducer: {
     area: makeUnitReducer,
+    auth: authDataReducer,
     [callUnit.reducerPath]: callUnitReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(callUnit.middleware),
