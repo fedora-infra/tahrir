7c3dbfbe92da4a09d004d0ab70b0b28834f5cb25
Shounak Dey <shounakdey@ymail.com>
2025-11-23 20:13:55 +0530

    Add POST endpoint for badge authorization management
    Co-authored-by: Shounak Dey <shounakdey@ymail.com>
    Co-authored-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

---
+++ tahrir/endpoints/admin/authorizations.py
@@ -0,0 +1,38 @@
+from flask import abort, g, jsonify, request
+
+from ...app import csrf, oidc
+from ...utils.user import require_admin
+from . import blueprint as bp
+
+
+@bp.route("/api/admin/authorization", methods=["POST"])
+@csrf.exempt
+@oidc.require_login
+@require_admin
+def add_authorization():
+    """Endpoint to add authorization and allow someone to admin a certain badge"""
+
+    data = request.get_json()
+    if not data:
+        return abort(400, "No details provided")
+
+    required_fields = ["badge_id", "user"]
+    for field in required_fields:
+        if not data.get(field):
+            return abort(400, f"No detail provided for {field!r}")
+
+    # ONE SHOULD NOT FEEL THE NEED OF USING THE EMAIL ADDRESS HERE
+    # THIS WORKAROUND IS TEMPORARY AND TAHRIR-API WOULD BE CHANGED TO ACCEPT JUST USERNAME
+    result = g.tahrirdb.add_authorization(
+        badge_id=data.get(required_fields[0]),
+        person_email=data.get(required_fields[1]) + "@fedoraproject.org",
+    )
+
+    if not result:
+        return abort(400, "Failed to add authorization")
+
+    return jsonify(
+        {
+            "message": f"Badge {data.get(required_fields[0])!r} authorized to {data.get(required_fields[1])!r}"
+        }
+    ), 201
