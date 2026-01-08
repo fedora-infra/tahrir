f7eb34edc17784edc60cac89d8972c424e0d19ad
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2026-01-09 00:51:23 +0530

    Include information on the project being associated with Red Hat
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

---
+++ frontend/src/components/footnote.jsx
@@ -0,0 +1,45 @@
+import { Col, Container, Image, Row } from "react-bootstrap";
+import { useSelector } from "react-redux";
+
+export default function FootNote() {
+  const load = useSelector((area) => area.area.load);
+  if (load) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <footer className="border-top py-2">
+        <Row className="align-items-center">
+          <Col className="col-lg-3 text-start">
+            <a
+              href="https://www.redhat.com/"
+              className="text-decoration-none"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="https://fedoraproject.org/assets/images/logo-sponsored-by-red-hat.png"
+                height="40"
+                className="d-block me-auto"
+              />
+            </a>
+          </Col>
+          <Col className="col-lg-9 text-end small text-muted">
+            <div>Fedora is sponsored by Red Hat. © 2025 Red Hat, Inc. and others</div>
+            <div>
+              <a
+                href="https://www.redhat.com/en/topics/linux/fedora-vs-red-hat-enterprise-linux"
+                className="text-decoration-none"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn more about the relationship between Red Hat and Fedora
+              </a>
+            </div>
+          </Col>
+        </Row>
+      </footer>
+    </Container>
+  );
+}
