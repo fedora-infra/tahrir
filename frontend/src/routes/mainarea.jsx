7813c824e5b9f5b3d707a68732a9ac0bd154e3c1
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2026-01-09 00:51:23 +0530

    Include information on the project being associated with Red Hat
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- frontend/src/routes/mainarea.jsx
+++ frontend/src/routes/mainarea.jsx
@@ -1,16 +1,18 @@
 import { Container } from "react-bootstrap";
 import { Outlet } from "react-router";
 
+import FootNote from "../components/footnote.jsx";
 import LoadNote from "../components/loadnote.jsx";
 import Navigate from "../components/navigate.jsx";
 
 export default function MainArea() {
   return (
-    <div>
+    <div className="d-flex flex-column min-vh-100">
       <Navigate />
-      <Container className="pt-2">
+      <Container className="pt-2 flex-grow-1">
         <Outlet />
       </Container>
+      <FootNote />
       <LoadNote />
     </div>
   );
