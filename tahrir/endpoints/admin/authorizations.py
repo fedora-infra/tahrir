f9eafb06f2d1de982258f5c32dcfb8c5e8a577cc
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2026-01-08 22:58:40 +0530

    Add REST API endpoint for removing needless authorizations
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- tahrir/endpoints/admin/authorizations.py
+++ tahrir/endpoints/admin/authorizations.py
@@ -36,3 +36,36 @@ def add_authorization():
             "message": f"Badge {data.get(required_fields[0])!r} authorized to {data.get(required_fields[1])!r}"
         }
     ), 201
+
+
+@bp.route("/api/admin/authorization", methods=["DELETE"])
+@csrf.exempt
+@oidc.require_login
+@require_admin
+def remove_authorization():
+    """Endpoint to remove authorization and revoke someone to admin a certain badge"""
+
+    data = request.get_json()
+    if not data:
+        return abort(400, "No details provided")
+
+    required_fields = ["badge_id", "user"]
+    for field in required_fields:
+        if not data.get(field):
+            return abort(400, f"No detail provided for {field!r}")
+
+    # ONE SHOULD NOT FEEL THE NEED OF USING THE EMAIL ADDRESS HERE
+    # THIS WORKAROUND IS TEMPORARY AND TAHRIR-API WOULD BE CHANGED TO ACCEPT JUST USERNAME
+    result = g.tahrirdb.delete_authorization(
+        badge_id=data.get(required_fields[0]),
+        person_email=data.get(required_fields[1]) + "@fedoraproject.org",
+    )
+
+    if not result:
+        return abort(404, "Authorization not found or failed to remove")
+
+    return jsonify(
+        {
+            "message": f"Badge {data.get(required_fields[0])!r} authorization revoked from {data.get(required_fields[1])!r}"
+        }
+    ), 200
