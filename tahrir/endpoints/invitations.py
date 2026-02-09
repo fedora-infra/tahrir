8b3a14be2ba253831a0404b2c50f717c7dad7571
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2026-02-09 22:38:32 +0530

    Make backend changes for listing profile invitations
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- tahrir/endpoints/invitations.py
+++ tahrir/endpoints/invitations.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from flask import abort, g, jsonify
 
 from ..app import csrf, oidc
+from ..utils.user import get_person
 from . import blueprint as bp
 
 
@@ -31,3 +32,42 @@ def claim_invitation(invitation_id: str):
     g.tahrirdb.add_assertion(claim.badge_id, g.oidc_user.person.email, datetime.now())
 
     return jsonify({"message": f"You have earned badge {claim.badge_id!r}"})
+
+
+@bp.route("/api/invitations/<string:user_id>", methods=["GET"])
+def get_invitations(user_id: str):
+    """Endpoint to list all invitations created by a given user."""
+
+    # TODO - Modify the endpoint to require authentication
+    # TODO - Remove the user_id path parameter requirement
+    person = get_person(user_id)
+    if not person:
+        return abort(404, f"User {user_id!r} not found")
+
+    invitations = g.tahrirdb.get_invitations(person_id=person.id)
+
+    if not invitations:
+        return abort(404, "No invitations available")
+
+    invitations_data = {}
+
+    for invitation in invitations:
+        badge = invitation.badge
+
+        if badge.id not in invitations_data:
+            invitations_data[badge.id] = {
+                "name": badge.name,
+                "image": badge.image,
+                "invitations": [],
+            }
+
+        invitations_data[badge.id]["invitations"].append(
+            {
+                "invitation_id": invitation.id,
+                "created_on": invitation.created_on.timestamp() if invitation.created_on else None,
+                "expires_on": invitation.expires_on.timestamp() if invitation.expires_on else None,
+                "expired": invitation.expired,
+            }
+        )
+
+    return jsonify(invitations_data)
