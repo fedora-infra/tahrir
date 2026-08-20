8be6f8d15897a19035d9295237accf9ebd0673fb
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2026-08-20 18:27:37 +0530

    Filter obsolete rarity tier from the rarities endpoint
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- tahrir/views/explore.py
+++ tahrir/views/explore.py
@@ -134,12 +134,17 @@ def json_rarities(rare=None):
         }
 
     if rare:
-        rarity_objc = g.tahrirdb.session.query(m.Rarity).filter_by(name=rare.upper()).first()
+        rarity_objc = (
+            g.tahrirdb.session.query(m.Rarity)
+            .filter(m.Rarity.name != "O")
+            .filter_by(name=rare.upper())
+            .first()
+        )
         if not rarity_objc:
             abort(404, "No such rarity")
         return jsonify([_rarity_data(b) for b in rarity_objc.badges if not b.legacy])
     else:
-        rarity_full = g.tahrirdb.session.query(m.Rarity).all()
+        rarity_full = g.tahrirdb.session.query(m.Rarity).filter(m.Rarity.name != "O").all()
         return jsonify(
             {r.name: [_rarity_data(b) for b in r.badges if not b.legacy] for r in rarity_full}
         )
